a2ce8fd4dec65002355e49838a9c248491131f4f
zhangzhao <zhangzhao@sensetime.com>
2021-09-08 18:42:07 +0800

    fix

---
+++ mk_server/app/server/server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/julienschmidt/httprouter"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type ZHttpServer struct {
+	Addr string
+	Timeout time.Duration
+	router *httprouter.Router
+}
+
+func NewZHttpServer(addr string, timeout int) *ZHttpServer {
+	return &ZHttpServer{
+		Addr: addr,
+		Timeout: time.Duration(timeout) * time.Second,
+	}
+}
+
+func (s *ZHttpServer) SetRouter(r *httprouter.Router)  {
+	s.router = r
+}
+
+func (s *ZHttpServer) StartServer() error {
+	serv := &http.Server{
+		Addr: s.Addr,
+		Handler: s.router,
+		ReadHeaderTimeout: s.Timeout,
+	}
+	return serv.ListenAndServe()
+}
+
+// handle
+func NewHttpRouter() *httprouter.Router {
+	r := httprouter.New()
+	r.HandlerFunc(http.MethodGet, "/hello", Hello)
+	return r
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	buf, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	println(buf)
+	res := struct {
+		Value string `json:"value"`
+	}{"hello"}
+	resData, err := json.Marshal(res)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(resData)
+}
---
+++ mk_server/go.mod
@@ -0,0 +1,8 @@
+module mk_server
+
+go 1.14
+
+require (
+	github.com/BurntSushi/toml v0.4.1
+	github.com/julienschmidt/httprouter v1.3.0
+)
